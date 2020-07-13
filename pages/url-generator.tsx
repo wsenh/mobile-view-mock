70ea363de0e8204a4bf88a238b89a8f0eb5098da
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-07-12 23:28:32 -0700

    Read query params in url-generator

--- pages/url-generator.tsx
+++ pages/url-generator.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Mock, Device } from "../components/Mock";
 import { MockOptions } from "../components/MockOptions";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const UrlGeneratorPage: React.FC = () => {
   const [src, setSrc] = useState("/embed-example");
   const [device, setDevice] = useState<Device>("device-iphone-x");
   const [title, setTitle] = useState("Mobile View Mock");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.mock_url) setSrc(router.query.mock_url as string);
+    if (router.query.device) setDevice(router.query.device as Device);
+    if (router.query.title) setTitle(router.query.title as string);
+  }, [router, setSrc, setDevice, setTitle]);
 
   return (
     <>
